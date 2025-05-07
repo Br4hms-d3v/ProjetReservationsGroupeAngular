bb7dac2960cbe87897cf038bee9c40d564ac59c8
Brahms <34168107+bramsobrahms@users.noreply.github.com>
2025-05-07 21:15:48 +0200

    add post method

--- src/app/service/show.service.ts
+++ src/app/service/show.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Show} from "../model/ShowModel";
+import {Observable} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -9,9 +10,13 @@ export class ShowService {
 
   private apiServerUrl = 'http://localhost:8080/api'
 
-  constructor(private _hpp: HttpClient) { }
+  constructor(private _http: HttpClient) { }
 
   getAllShows() {
-    return this._hpp.get<Show[]>(`${this.apiServerUrl}/shows`)
+    return this._http.get<Show[]>(`${this.apiServerUrl}/shows`);
+  }
+
+  addShow(show: Show): Observable<Show> {
+    return this._http.post<Show>(`${this.apiServerUrl}/admin/show`, show);
   }
 }
