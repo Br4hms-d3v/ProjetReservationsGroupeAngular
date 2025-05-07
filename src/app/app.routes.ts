637b6ea62a1cdaaf93088d47a792f9a4714ea2f6
Brahms <34168107+bramsobrahms@users.noreply.github.com>
2025-05-07 03:10:56 +0200

    Route to navigate on my Shows

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import {ShowComponent} from './show/show.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: '', component: ShowComponent},
+  {path: 'spectacles', component: ShowComponent}
+];
