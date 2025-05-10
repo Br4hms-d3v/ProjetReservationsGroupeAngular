d0048dc785d0650081cc1ef2322e79852fd61b5a
Brahms <34168107+bramsobrahms@users.noreply.github.com>
2025-05-10 19:39:12 +0200

    Add route for error and route for show by id

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,11 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
 import {ShowComponent} from './show/show.component';
+import {NotFoundPageComponent} from './not-found-page/not-found-page.component';
+import {ShowIdComponent} from './show-id/show-id.component';
 
 export const routes: Routes = [
   {path: '', component: ShowComponent},
-  {path: 'spectacles', component: ShowComponent}
+  {path: 'spectacles', component: ShowComponent},
+  {path: 'spectacle/:id', component: ShowIdComponent},
+  {path: '**', component: NotFoundPageComponent}
 ];
