bb6481c3ded19e72441556c2be8dd538c42911e1
Brahms <34168107+bramsobrahms@users.noreply.github.com>
2025-05-12 01:59:52 +0200

    Add route for update Show

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import {Routes} from '@angular/router';
 import {ShowComponent} from './show/show.component';
 import {NotFoundPageComponent} from './not-found-page/not-found-page.component';
 import {ShowIdComponent} from './show-id/show-id.component';
+import {ShowEditComponent} from './show-edit/show-edit.component';
 
 export const routes: Routes = [
   {path: '', component: ShowComponent},
   {path: 'spectacles', component: ShowComponent},
   {path: 'spectacle/:id', component: ShowIdComponent},
+  {path: 'mise-a-jour-spectacle/:id', component: ShowEditComponent},
   {path: '**', component: NotFoundPageComponent}
 ];
